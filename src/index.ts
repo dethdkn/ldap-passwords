59341373b7b092e84e932a917c284a4493dd0c3c
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-24 10:49:09 -0300

    ✨ Added verify all function

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,7 @@ import { ssha, verifySSHA } from 'ldap-sha'
 import { sha512Crypt, verifySha512 } from 'ldap-sha512'
 
 export { md5, verifyMD5, ssha, verifySSHA, sha512Crypt, verifySha512 }
+
+export function ldapVerifyAll(textPassword: string, hashedPassword: string | string[]): boolean {
+	return verifyMD5(textPassword, hashedPassword) || verifySSHA(textPassword, hashedPassword) || verifySha512(textPassword, hashedPassword)
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -3,3 +3,16 @@ import type { ssha, verifySSHA } from 'ldap-sha/types/index'
 import type { sha512Crypt, verifySha512 } from 'ldap-sha512/types/index'
 
 export { md5, verifyMD5, ssha, verifySSHA, sha512Crypt, verifySha512 }
+
+/**
+ * @function
+ * Verify passwords encrypted with MD5, SSHA or SHA512 CRYPT.
+ *
+ * @param {string} textPassword - Plain text password.
+ * @param {string | string[]} hashedPassword - String or Array of strings to be compared to the plain text password.
+ *
+ * @example
+ * const isValid = ldapVerifyAll('mySuperSecretPassword', arrayOfHashedPasswords)
+ * // return true or false
+ */
+export declare function ldapVerifyAll(textPassword: string, hashedPassword: string | string[]): boolean
