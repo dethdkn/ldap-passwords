d5fecceacd996e8ba566df28ba09ab758d502f48
Gabriel Rosa <gabriel@rosa.dev.br>
2024-01-25 14:41:45 -0300

    🐛 Fixed types import

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,6 +1,6 @@
-import type { md5, verifyMD5 } from 'ldap-md5/types/index'
-import type { ssha, verifySSHA } from 'ldap-sha/types/index'
-import type { sha512Crypt, verifySha512 } from 'ldap-sha512/types/index'
+import { md5, verifyMD5 } from 'ldap-md5/types/index'
+import { ssha, verifySSHA } from 'ldap-sha/types/index'
+import { sha512Crypt, verifySha512 } from 'ldap-sha512/types/index'
 
 export { md5, verifyMD5, ssha, verifySSHA, sha512Crypt, verifySha512 }
 
